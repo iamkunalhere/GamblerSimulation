85fb02dcfe13cf1f01784923296e82a2129abdc5
kunal jadhav <iamkunalhere@gmail.com>
2020-03-07 12:01:30 +0530

    Gambler wins or lost checking

--- GamblerSimulation.sh
+++ GamblerSimulation.sh
@@ -5,6 +5,17 @@
 #Author : Kunal Jadhav
 #Date : 7 March 2020
 
-everydayStakes=100
+dayStakes=100
 bet=1
 days=0
+
+function winOrLoose() {
+	state=$((RANDOM%2))
+	if  [[ $state -eq 1 ]]
+	 then
+		dayStakes=$(($dayStakes+1))
+	 else
+		dayStakes=$(($dayStakes-1))
+	fi
+}
+winOrLoose
