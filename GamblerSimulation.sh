55fb18ffe5f6b44278e3eda3f31868ab0a2d3a4a
kunal jadhav <iamkunalhere@gmail.com>
2020-03-07 11:44:37 +0530

    Gambler starts with everyday stakes

---
+++ GamblerSimulation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+#Problem Statement : Gambler Simulation
+#Discription : This problem simulates a Gambler. Being a Calculative Gambler exits if the Stake reaches a high or a low limit.
+#Author : Kunal Jadhav
+#Date : 7 March 2020
+
+everydayStakes=100
+bet=1
+days=0
